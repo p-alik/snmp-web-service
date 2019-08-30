406024598df64b4ac507b2bec8bc5e03c68eb98b
Alexei Pastuchov <info@maximka.de>
2019-08-30 18:37:31 +0200

    throw 404 if snmp request failed

--- src/App.hs
+++ src/App.hs
@@ -1,32 +1,23 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
 module App
   ( runApp
   )
 where
 
-import           App.Parser                               ( IPv4T(..)
-                                                          , ObjectIdentifierT(..)
-                                                          )
-import           App.Settings                             ( Options(..) )
-import           App.Snmp                                 ( SnmpResponseT )
-import qualified App.Snmp                                 ( snmpGet )
+import           App.Parser                 (IPv4T (..), ObjectIdentifierT (..))
+import           App.Settings               (Options (..))
+import           App.Snmp                   (SnmpResponseT)
+import qualified App.Snmp                   (snmpGet)
 
-import           Control.Monad.IO.Class                   ( MonadIO
-                                                          , liftIO
-                                                          )
-import           Network.Wai.Handler.Warp                 ( run )
-import           Servant                                  ( Application
-                                                          , Handler
-                                                          , Proxy(..)
-                                                          , Server
-                                                          , serve
-                                                          )
-import           Servant.API                              ( Capture
-                                                          , Get
-                                                          , JSON
-                                                          , (:>)
-                                                          )
+import qualified Data.ByteString.Lazy.Char8 as B (pack)
+
+import           Control.Monad.IO.Class     (MonadIO, liftIO)
+import           Network.Wai.Handler.Warp   (run)
+import           Servant                    (Application, Handler, Proxy (..),
+                                             Server, serve, throwError)
+import           Servant.API                ((:>), Capture, Get, JSON)
+import           Servant.Server
 
 type SnmpAPI = "snmpget"  :> Capture "ip" IPv4T :> Capture "oid" ObjectIdentifierT :> Get '[JSON] SnmpResponseT
 
@@ -40,7 +31,7 @@ snmpGet :: Options -> IPv4T -> ObjectIdentifierT -> Handler SnmpResponseT
 snmpGet o (IPv4T ip) (ObjectIdentifierT oid) = do
   v <- liftIO $ App.Snmp.snmpGet (roCommunity o) oid ip
   case v of
-    Left  e -> error (show e)
+    Left  e -> throwError (err404 {errBody = B.pack $ show e})
     Right r -> return r
 
 app :: Options -> Application
