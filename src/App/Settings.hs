e4ab2c5d223f44e2ddc67f71702c68eb21746252
Alexei Pastuchov <info@maximka.de>
2019-09-08 21:43:00 +0200

    add snmp timeout and retries into options record

--- src/App/Settings.hs
+++ src/App/Settings.hs
@@ -4,13 +4,16 @@ module App.Settings
   )
 where
 
-import           Data.ByteString                          ( ByteString )
-import           Data.Semigroup                           ( (<>) )
+import           Data.ByteString                 (ByteString)
+import           Data.Semigroup                  ((<>))
 
 import           Options.Applicative
+import           Options.Applicative.Help.Pretty (text, vsep)
 
 data Options = Options {
     roCommunity :: ByteString
+  , timeout     :: Int
+  , retries     :: Int
 } deriving Show
 
 
@@ -19,6 +22,11 @@ parserOptions = Options <$> strOption
   (long "readonly-community" <> metavar "SNMP-COMMUNITY-STRING" <> help
     "community for SNMP read only requests"
   )
+  <*> oa "timeout" "SNMP request timeout in seconds."
+  <*> oa "retry" "Number of retries to be used in SNMP request."
+  where
+    oa n h = option auto (long n <> metavar "1" <> value 1 <> hd (h:["Default 1"]))
+    hd = helpDoc . Just . vsep . map text
 
 parseOptions :: ParserInfo Options
 parseOptions =
